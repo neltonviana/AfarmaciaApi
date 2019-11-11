c4f72700b9def19a365710cdaae294fd3eda8198
NELTON VIANA <nelton.viana@centavo.co.mz>
2019-11-11 14:49:32 +0200

    Submissão completa da Aplicação(API)

---
+++ AfarmaciaApi/AfarmaciaApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AfarmaciaApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ AfarmaciaApi/AfarmaciaApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AfarmaciaApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
